9a44b6dc8add2d767d43194a33ddf035426107e8
bepyan <bepyan@naver.com>
2021-02-18 03:07:40 +0900

    [add] styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+`
 
 function Header() {
-  return <div>header</div>;
+  return <Title>Header</Title>;
 }
 
 export default Header;
--- src/components/home/TmpList.js
+++ src/components/home/TmpList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 80%;
+  margin: auto;
+`
 
 function TmpList() {
-  return <div>tmp list</div>;
+  const arr = ['a', 'b', 'c', 'd']
+  const render = () => arr.map(item => <p> item </p>)
+
+  return <Wrapper>
+    {render()}
+  </Wrapper>;
 }
 
 export default TmpList;
