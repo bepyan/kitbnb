1c8bf51cac5ea6aeba652fd459eda8964ab167be
bepyan <bepyan@naver.com>
2021-04-17 23:09:38 +0900

    Updates

---
+++ precache-manifest.5b30fcbb56b2b8e71e694e4c0aa3ea3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8c5d70f7a56cf29e614933580aac96",
+    "url": "/kitbnb/index.html"
+  },
+  {
+    "revision": "f846ed2a078f0c0c3e0f",
+    "url": "/kitbnb/static/js/2.cf91db0a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/kitbnb/static/js/2.cf91db0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db293d9e0cd02067d551",
+    "url": "/kitbnb/static/js/main.f86ce72e.chunk.js"
+  },
+  {
+    "revision": "d9d530fbccc482026a29",
+    "url": "/kitbnb/static/js/runtime-main.fa2b50af.js"
+  }
+]);
